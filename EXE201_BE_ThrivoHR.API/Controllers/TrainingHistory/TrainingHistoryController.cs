f1107890d059b7b4824fed16c4ec134df1f04c75
Phuc Nghi <meocodien1762003@gmail.com>
2024-09-02 01:10:04 +0700

    Add filtering to TrainingHistoryController
    Introduced a new `Get` endpoint in `TrainingHistoryController` to allow querying training histories using the `FilterTrainingHistory` model. Reformatted the return statement in `CreateTrainingHistory` for consistency. Added `FilterTrainingHistory.cs` to define filtering criteria and query handler. Updated `EXE201_BE_ThrivoHR.Application.csproj` to remove folder inclusion for `UseCase\V1\TrainingHistories\Queries\`, indicating a reorganization.

--- EXE201_BE_ThrivoHR.API/Controllers/TrainingHistory/TrainingHistoryController.cs
+++ EXE201_BE_ThrivoHR.API/Controllers/TrainingHistory/TrainingHistoryController.cs
@@ -1,6 +1,7 @@
 ﻿
 using EXE201_BE_ThrivoHR.Application.Model;
 using EXE201_BE_ThrivoHR.Application.UseCase.V1.TrainingHistories.Commands;
+using EXE201_BE_ThrivoHR.Application.UseCase.V1.TrainingHistories.Queries;
 
 namespace EXE201_BE_ThrivoHR.API.Controllers.TrainingHistory;
 
@@ -10,7 +11,13 @@ public class TrainingHistoryController(ISender sender) : BaseController(sender)
     public async Task<ActionResult<Result>> CreateTrainingHistory([FromBody] CreateTrainingHistory model)
     {
         var result = await _sender.Send(model);
-        return result.IsSuccess ?result : BadRequest(result);
+        return result.IsSuccess ? result : BadRequest(result);
+    }
+    [HttpGet]
+    public async Task<ActionResult<Result>> Get([FromQuery] FilterTrainingHistory model)
+    {
+        var result = await _sender.Send(model);
+        return result.IsSuccess ? result : BadRequest(result);
     }
 }
 
---
+++ EXE201_BE_ThrivoHR.Application/UseCase/V1/TrainingHistories/Queries/FilterTrainingHistory.cs
@@ -0,0 +1,48 @@
+﻿using EXE201_BE_ThrivoHR.Domain.Common.Status;
+using EXE201_BE_ThrivoHR.Domain.Entities;
+
+namespace EXE201_BE_ThrivoHR.Application.UseCase.V1.TrainingHistories.Queries;
+
+public record FilterTrainingHistory(
+    string? EmployeeCode,
+    DateOnly? StartDay,
+    string? WorkshopName,
+    string? Content,
+    TrainingHistoryEnum? Status,
+    int PageSize = 100,
+    int PageNumber = 1
+    ) : IQuery<PagedResult<TrainingHistoryDto>>;
+
+internal sealed class FilterTrainingHistoryQueryHandler : IQueryHandler<FilterTrainingHistory, PagedResult<TrainingHistoryDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ITrainingHistoryRepository _trainingHistoryRepository;
+
+    public FilterTrainingHistoryQueryHandler(IMapper mapper, ITrainingHistoryRepository trainingHistoryRepository)
+    {
+        _mapper = mapper;
+        _trainingHistoryRepository = trainingHistoryRepository;
+    }
+
+    public async Task<Result<PagedResult<TrainingHistoryDto>>> Handle(FilterTrainingHistory request, CancellationToken cancellationToken)
+    {
+        IQueryable<TrainingHistory> filter(IQueryable<TrainingHistory> x)
+        {
+
+            x = x.Where(x =>
+                 (string.IsNullOrEmpty(request.EmployeeCode) || x.Employee!.EmployeeCode == request.EmployeeCode)
+                 && (!request.StartDay.HasValue || x.StartDay == request.StartDay)
+                 && (string.IsNullOrEmpty(request.WorkshopName) || x.WorkshopName.Contains(request.WorkshopName))
+                 && (string.IsNullOrEmpty(request.Content) || x.Content.Contains(request.Content))
+                 && (!request.Status.HasValue || x.Status == request.Status)
+             );
+
+            return x;
+        }
+        var list = await _trainingHistoryRepository.FindAllAsync(request.PageNumber, request.PageSize, filter, cancellationToken);
+        var mapped = PagedResult<TrainingHistoryDto>.Create(list.TotalCount, list.PageCount, list.PageSize, list.PageNo, list.MapToTrainingHistoryListDto(_mapper));
+        return Result.Success(mapped);
+
+    }
+}
+
