364c0c64a1f988fc2544ff32006eba45495f3ea5
Phuc Nghi <meocodien1762003@gmail.com>
2024-09-20 13:13:17 +0700

    Refactor Authentication and add IP retrieval
    Updated Authentication class to include ICurrentUserService for IP retrieval. Moved and restructured Authenticate and RefreshToken methods with HTTP POST attributes. Added Get method to return current user's IP. Enhanced CurrentUserService with IHttpContextAccessor for IP access and added IP property. Updated ICurrentUserService interface to include IP property.

--- EXE201_BE_ThrivoHR.API/Controllers/Authenticate/Authentication.cs
+++ EXE201_BE_ThrivoHR.API/Controllers/Authenticate/Authentication.cs
@@ -1,33 +1,32 @@
 ﻿using EXE201_BE_ThrivoHR.Application.Model;
 using EXE201_BE_ThrivoHR.Application.UseCase.Authentication;
+using EXE201_BE_ThrivoHR.Domain.Services;
 
-namespace EXE201_BE_ThrivoHR.API.Controllers.Authenticate
+namespace EXE201_BE_ThrivoHR.API.Controllers.Authenticate;
+public class Authentication(ISender sender, ICurrentUserService currentUserService) : BaseController(sender)
 {
-
-    public class Authentication(ISender sender) : BaseController(sender)
+   
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Authenticate([FromBody] LoginQuery loginQuery)
     {
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Authenticate([FromBody] LoginQuery loginQuery)
-        {
-            var result = await _sender.Send(new LoginQuery(loginQuery.EmployeeCode, loginQuery.Password));
-            return Ok(result);
-        }
-        [HttpPost("refresh")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> RefreshToken([FromBody] TokenModel tokenModel)
-        {
-            var result = await _sender.Send(new RefreshTokenQuery(tokenModel));
-            return Ok(result);
-        }
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok("Hello");
-        }
+        var result = await _sender.Send(new LoginQuery(loginQuery.EmployeeCode, loginQuery.Password));
+        return Ok(result);
+    }
+    [HttpPost("refresh")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> RefreshToken([FromBody] TokenModel tokenModel)
+    {
+        var result = await _sender.Send(new RefreshTokenQuery(tokenModel));
+        return Ok(result);
+    }
+    [HttpGet]
+    public IActionResult Get()
+    {
+       return Ok(currentUserService.IP);
     }
 }
--- EXE201_BE_ThrivoHR.Domain/Services/ICurrentUserService.cs
+++ EXE201_BE_ThrivoHR.Domain/Services/ICurrentUserService.cs
@@ -4,6 +4,7 @@ public interface ICurrentUserService
 {
     string? UserId { get; }
     string? UserName { get; }
+    string? IP { get; }
     Task<bool> IsInRoleAsync(string role);
     Task<bool> AuthorizeAsync(string policy);
 }
