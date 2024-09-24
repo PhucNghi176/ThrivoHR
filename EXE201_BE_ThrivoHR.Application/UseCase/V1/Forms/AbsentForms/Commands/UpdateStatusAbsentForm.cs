c8c78f3d0a334a1196b1ca4c8f91f9c9ac35a685
Phuc Nghi <meocodien1762003@gmail.com>
2024-09-25 02:14:37 +0700

    Add ICurrentUserService to set ApproverId in form handler
    The UpdateStatusAbsentFormHandler class now includes a new
    dependency, ICurrentUserService, in its constructor. This service
    is used to fetch the current user's ID. The Handle method has
    been updated to set the ApproverId of the form to the current
    user's ID, ensuring the approver is recorded as the user making
    the update.

--- EXE201_BE_ThrivoHR.Application/UseCase/V1/Forms/AbsentForms/Commands/UpdateStatusAbsentForm.cs
+++ EXE201_BE_ThrivoHR.Application/UseCase/V1/Forms/AbsentForms/Commands/UpdateStatusAbsentForm.cs
@@ -1,15 +1,17 @@
 ﻿using EXE201_BE_ThrivoHR.Domain.Common.Status;
+using EXE201_BE_ThrivoHR.Domain.Services;
 using static EXE201_BE_ThrivoHR.Application.Common.Exceptions.Employee;
 
 namespace EXE201_BE_ThrivoHR.Application.UseCase.V1.Forms.AbsentForms.Commands;
 
 public record UpdateStatusAbsentForm(string ID, FormStatus Status) : ICommand;
-internal sealed class UpdateStatusAbsentFormHandler(IAbsentFormRepository _absentFormRepository) : ICommandHandler<UpdateStatusAbsentForm>
+internal sealed class UpdateStatusAbsentFormHandler(IAbsentFormRepository _absentFormRepository,ICurrentUserService _currentUserService) : ICommandHandler<UpdateStatusAbsentForm>
 {
     public async Task<Result> Handle(UpdateStatusAbsentForm request, CancellationToken cancellationToken)
     {
         var form = await _absentFormRepository.FindAsync(x => x.Id == request.ID, cancellationToken) ?? throw new NotFoundException("Not found");
         form.Status = request.Status;
+        form.ApproverId = _currentUserService.UserId;
         await _absentFormRepository.UpdateAsync(form);
         await _absentFormRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
 
