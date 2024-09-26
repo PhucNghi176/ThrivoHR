d41488b45f2296c698965463429fac09d8828f9f
Phuc Nghi <meocodien1762003@gmail.com>
2024-09-26 16:06:09 +0700

    Add filtering logic to FilterRewardAndDisciplinaryHandler
    - Added a new using directive for `EXE201_BE_ThrivoHR.Domain.Entities`.
    - Introduced a new optional parameter `IsRewards` in the `FilterRewardAndDisciplinary` record.
    - Implemented a new local function `filter` within the `FilterRewardAndDisciplinaryHandler` class to apply filtering logic based on the `EmployeeCode`, `IsRewards`, `FormOfAction`, and `Status` properties of the request.
    - Modified the call to `rewardAndDisciplinaryRepository.FindAllAsync` to include the new `filter` function for applying the query filters.

--- EXE201_BE_ThrivoHR.Application/UseCase/V1/RewardAndDisciplinarys/Queries/FilterRewardAndDisciplinary.cs
+++ EXE201_BE_ThrivoHR.Application/UseCase/V1/RewardAndDisciplinarys/Queries/FilterRewardAndDisciplinary.cs
@@ -1,11 +1,13 @@
 ﻿using EXE201_BE_ThrivoHR.Application.Common.Exceptions;
 using EXE201_BE_ThrivoHR.Application.Common.Method;
 using EXE201_BE_ThrivoHR.Domain.Common.Status;
+using EXE201_BE_ThrivoHR.Domain.Entities;
 
 namespace EXE201_BE_ThrivoHR.Application.UseCase.V1.RewardAndDisciplinarys.Queries;
 
 public record FilterRewardAndDisciplinary(
     string? EmployeeCode,
+    bool? IsRewards,
     FormOfAction? FormOfAction,
     FormStatus? Status,
    int PageSize = 100,
@@ -14,11 +16,21 @@ internal sealed class FilterRewardAndDisciplinaryHandler(IRewardAndDisciplinaryR
 {
     public async Task<Result<PagedResult<RewardAndDisciplinaryDto>>> Handle(FilterRewardAndDisciplinary request, CancellationToken cancellationToken)
     {
-       
+        IQueryable<RewardsAndDisciplinary> filter(IQueryable<RewardsAndDisciplinary> x)
+        {
+            x = x.Where(x =>
+                 (string.IsNullOrEmpty(request.EmployeeCode) || x.EmployeeId == request.EmployeeCode ||
+                 (request.IsRewards == null || x.IsRewards == request.IsRewards) &&
+                 (request.FormOfAction == null || x.FormOfAction == request.FormOfAction) &&
+                 (request.Status == null || x.Status == request.Status)));
+            return x;
+        }
+
         var result = await rewardAndDisciplinaryRepository.FindAllAsync(
-            
+
             request.PageNumber,
             request.PageSize,
+            filter,
             cancellationToken);
         return Result.Success(PagedResult<RewardAndDisciplinaryDto>.Create(result.TotalCount, result.PageCount, result.PageSize, result.PageNo, result.MapToListRewardAndDisciplinaryDto(mapper)));
     }
