80c73431befbfd454869c5826e29998625394dbb
Phuc Nghi <meocodien1762003@gmail.com>
2024-09-28 15:38:36 +0700

    Add repository interfaces for Project, Task, and History
    Introduce IProjectRepository, IProjectTaskRepository, and ITaskHistoryRepository in EXE201_BE_ThrivoHR.Domain.Repositories namespace. Each interface extends IEFRepository with specific entity types: Project, ProjectTask, and TaskHistory. Added necessary using directives for EXE201_BE_ThrivoHR.Domain.Entities.

---
+++ EXE201_BE_ThrivoHR.Domain/Repositories/IProjectRepository.cs
@@ -0,0 +1,3 @@
+﻿using EXE201_BE_ThrivoHR.Domain.Entities;
+namespace EXE201_BE_ThrivoHR.Domain.Repositories;
+public interface IProjectRepository : IEFRepository<Project, Project>;
---
+++ EXE201_BE_ThrivoHR.Domain/Repositories/IProjectTaskRepository.cs
@@ -0,0 +1,3 @@
+﻿using EXE201_BE_ThrivoHR.Domain.Entities;
+namespace EXE201_BE_ThrivoHR.Domain.Repositories;
+public interface IProjectTaskRepository : IEFRepository<ProjectTask, ProjectTask>;
