14f8ad000fa41bf6e1490baef66e914d111ea2be
Phuc Nghi <meocodien1762003@gmail.com>
2024-09-28 15:53:46 +0700

    Add ProjectDto and mapping extensions; update csproj
    Updated `EXE201_BE_ThrivoHR.Application.csproj` to include new folders for project commands and queries under `UseCase\V1\Projects`. Cleaned up redundant using directives in `Project.cs`. Added `ProjectDto.cs` defining `ProjectDto` class with AutoMapper mapping. Added `ProjectDtoMappingExtension.cs` for mapping `Project` entities to `ProjectDto` objects.

---
+++ EXE201_BE_ThrivoHR.Application/UseCase/V1/Projects/ProjectDto.cs
@@ -0,0 +1,29 @@
+﻿using EXE201_BE_ThrivoHR.Application.Common.Mappings;
+using EXE201_BE_ThrivoHR.Domain.Entities;
+namespace EXE201_BE_ThrivoHR.Application.UseCase.V1.Projects;
+public class ProjectDto : IMapFrom<Project>
+{
+    public new string Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? LeaderName { get; set; }
+    public int TotalEmployee { get; set; }
+    public int TotalTask { get; set; }
+    public string? SubLeaderName { get; set; }
+    public double Progress { get; set; }
+    public string? Status { get; set; }
+
+    public static ProjectDto Create(Project project, IMapper mapper)
+    {
+        return mapper.Map<ProjectDto>(project);
+    }
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Project, ProjectDto>()
+            .ForMember(d => d.LeaderName, opt => opt.MapFrom(s => s.Leader.FullName))
+            .ForMember(d => d.SubLeaderName, opt => opt.MapFrom(s => s.SubLeader.FullName))
+            .ForMember(d => d.TotalEmployee, opt => opt.MapFrom(s => s.Employees.Count))
+            .ForMember(d => d.TotalTask, opt => opt.MapFrom(s => s.Tasks.Count))
+            .ForMember(d => d.Status, opt => opt.MapFrom(s => s.Status.ToString())).ReverseMap();
+    }
+}
---
+++ EXE201_BE_ThrivoHR.Application/UseCase/V1/Projects/ProjectDtoMappingExstension.cs
@@ -0,0 +1,11 @@
+﻿using EXE201_BE_ThrivoHR.Domain.Entities;
+
+namespace EXE201_BE_ThrivoHR.Application.UseCase.V1.Projects;
+
+public static class ProjectDtoMappingExstension
+{
+    public static ProjectDto ToProjectDto(this Project project, IMapper mapper)
+    => mapper.Map<ProjectDto>(project);
+    public static IEnumerable<ProjectDto> ToListProjectDtos(this IEnumerable<Project> projects, IMapper mapper) =>
+        mapper.Map<IEnumerable<ProjectDto>>(projects);
+}
